7d21ce7f363fc971748bfed6a29373e3a662fdfe
fransanda <82968969+fransanda@users.noreply.github.com>
2021-08-16 19:23:56 +0100

    Update main[1].js

--- main[1].js
+++ main[1].js
@@ -1,12 +1,16 @@
 
  
   var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1cyRSNprW-B0YG_avA9SLJfAN3hFiNZoMB_eHBgXpduU/edit?usp=sharing';
-  
-  function init() {
-    Tabletop.init( { key: publicSpreadsheetUrl,
-                     callback: showInfo,
-                     simpleSheet: true } )
-  }
+
+function init() {
+          Papa.parse(publicSpreadsheetUrl, {
+          download: true,
+          header: true,
+          complete: showInfo(results) {
+            var data = results.data
+            console.log(data)
+          }
+        })
 
  
 function showInfo(data, tabletop) {
